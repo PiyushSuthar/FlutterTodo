30e8122b64f476aa1996fc7e177d3bd35b9e32f4
Piyush Suthar <piyushsuthar100@gmail.com>
2023-07-17 19:39:07 +0530

    init: Todo Project

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Todo App",
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Todo App')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Get Your Shit Together LOL!'),
+        ],
+      ),
+    );
+  }
+}
